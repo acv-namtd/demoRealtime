713aa486f54b7fb2d9661fed32dbfac9a86db3b4
Nam Tran Dinh <namtd@atmarkcafe.org>
2018-10-21 22:22:59 +0700

    push project

---
+++ app/Http/Controllers/demoRealtimeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Pusher\Pusher;
+
+class demoRealtimeController extends Controller
+{
+    public function index()
+    {
+    	return view("demoRealtime.index");
+    }
+    public function send()
+    {
+    	return view("demoRealtime.index");
+    }
+    public function sendMessage(Request $request)
+    {
+    	$data = stdClass();
+    	$data->title = $request->title;
+    	$data->message = $request->message;
+    	$options = array(
+            'cluster' => 'mt1',
+            'encrypted' => true
+        );
+        $pusher = new Pusher(
+            env('PUSHER_APP_KEY'),
+            env('PUSHER_APP_SECRET'),
+            env('PUSHER_APP_ID'),
+            $options
+        );
+        $pusher->trigger('demoRealtime', 'demoRealtime', $data);
+        return redirect()->route('send');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get("index","demoRealtimeController@index");
+Route::get("send","demoRealtimeController@send")->name("send");
+Route::post("send","demoRealtimeController@sendMessage");
